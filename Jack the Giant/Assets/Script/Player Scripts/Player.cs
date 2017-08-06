2e15f02b5fd3676714ed0eb1fc5c94e240b27130
Ian Sikora <ianjsikora@Ians-MacBook-Pro.local>
2017-08-06 14:27:30 +0800

    Up to lesson 3.22

---
+++ Jack the Giant/Assets/Script/Player Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f, maxVelocity = 4f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Using FixedUpate instead of Update. Update is called every frame. Fixed updated is called every few frames.
+	void FixedUpdate () {
+		PlayerMoveKeyboard ();
+	}
+
+	void PlayerMoveKeyboard () {
+		float forceX = 0f;
+		float vel = Mathf.Abs (myBody.velocity.x);
+
+		// If negative, user input is left. If positive, user input is right.
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (h > 0) {
+
+			if (vel < maxVelocity) {
+				forceX = speed;
+			}
+				
+			Vector3 temp = transform.localScale;
+			temp.x = 1.2f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+			
+		} else if (h < 0) {
+
+			if (vel < maxVelocity) {
+				forceX = -speed;
+			}
+
+			Vector3 temp = transform.localScale;
+			temp.x = -1.2f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+			
+		} else {
+
+			anim.SetBool ("Walk", false);
+		
+		}
+
+		myBody.AddForce (new Vector2 (forceX, 0));
+	
+	}
+
+}
